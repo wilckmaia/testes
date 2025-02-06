11244b11b3e6f689fabf541365e1c45261a11214
wilckmaia <153649327+wilckmaia@users.noreply.github.com>
2025-02-06 22:18:32 +0000

    Cria uma API REST simples

---
+++ node/index3.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/api" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Olá, esta é uma API básica!" }));
+  } else {
+    res.writeHead(404);
+    res.end("Rota não encontrada");
+  }
+});
+
+server.listen(3000, () => {
+  console.log("API rodando em http://localhost:3000");
+});
